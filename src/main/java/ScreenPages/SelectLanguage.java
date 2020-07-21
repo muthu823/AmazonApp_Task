0962330381449371cc732ff3ac7f76b883ccd584
Lakshmi <Lakshmi@DESKTOP-NDI32SC>
2020-07-21 08:30:28 +0530

    Amazon shopping Test Automation Script. Functionality included, Login Test, Scroll, Buy TV.

---
+++ src/main/java/ScreenPages/SelectLanguage.java
@@ -0,0 +1,38 @@
+package ScreenPages;
+
+
+
+import java.util.List;
+
+import org.openqa.selenium.support.PageFactory;
+
+import BasePackage.ScreenBase;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+public class SelectLanguage extends ScreenBase
+{
+
+	@AndroidFindBy(xpath="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout[2]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ViewAnimator/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.GridLayout/android.widget.ImageView[1]")
+	public MobileElement English;
+	
+	//@AndroidFindBy(className ="android.widget.GridLayout")
+	//public List<AndroidElement> English;
+	
+	
+	public SelectLanguage(AppiumDriver<MobileElement> driver)
+	{
+		super(driver);
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+
+	public void language()
+	{
+		English.click();
+
+	}
+
+}
